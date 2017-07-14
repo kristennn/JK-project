396266a72e6ef4b5e7c8f78754d823441562a065
zpf0918 <zhanggongzifei@163.com>
2017-07-14 12:16:52 +0800

    user can delte owe posts

--- app/controllers/account/posts_controller.rb
+++ app/controllers/account/posts_controller.rb
@@ -8,4 +8,11 @@ class Account::PostsController < ApplicationController
   def edit
     @post = Post.find_by_friendly_id!(params[:id])
   end
+
+  def destroy
+    @post = Post.find_by_friendly_id!(params[:id])
+    @post.destroy
+    flash[:alert] = "已成功删除该文章"
+    redirect_to :back
+  end
 end
